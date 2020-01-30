baf597a60bd443054184f66739b1c52c846e7d59
sandra <sandra.luengo@evoluciona.com>
2020-01-30 12:10:40 +0100

    snapshot added to reducers tests

--- src/redux/reducers/changeColor/changeColorReducer.test.js
+++ src/redux/reducers/changeColor/changeColorReducer.test.js
@@ -4,14 +4,14 @@ describe('TOGGLE_COLOR', () => {
   it('initialState is correct', () => {
     const action = { type: 'TESTING_TOGGLE_COLOR' };
     const initialState = { toggleColor: false };
-    expect(changeColorReducer(undefined, action)).toEqual(initialState);
-    // expect(simpleReducer(undefined, action).contact).toMatchSnapshot();
+    // expect(changeColorReducer(undefined, action)).toEqual(initialState);
+    expect(changeColorReducer(undefined, action).contact).toMatchSnapshot();
   });
   it('returns the correct state', () => {
     const action = { type: 'TOGGLE_COLOR', payload: true };
     const expectedState = { toggleColor: true };
-    expect(changeColorReducer(undefined, action)).toEqual(expectedState);
-    // expect(simpleReducer(undefined, action)).toMatchSnapshot();
+    // expect(changeColorReducer(undefined, action)).toEqual(expectedState);
+    expect(changeColorReducer(undefined, action)).toMatchSnapshot();
   });
 });
 
